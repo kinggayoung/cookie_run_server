aa62ce665697bb51ab57b69df1a933261315ec9e
gayoung <gayoung1479@naver.co>
2021-04-20 19:47:07 +0900

    프로젝트 등록

---
+++ src/main/java/com/gayoung/cookie_run/CookieRunApplication.java
@@ -0,0 +1,13 @@
+package com.gayoung.cookie_run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieRunApplication.class, args);
+	}
+
+}
